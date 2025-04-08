af0df7895eb69233801f6fae8fc330eb357f42ea
Rajat Kumar <rajatky3107@gmail.com>
2025-04-08 21:19:55 +0530

    Update dependencies and enhance styling in the React app. Added new packages for UI components and animations, including @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, tailwind-variants, and tw-animate-css. Refactored App.css for improved theming and layout, and updated App.tsx to include a logo and new navigation structure.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
+import logoTransparent from "./assets/logo-transparent.png";
 import "./App.css";
 
 function App() {
@@ -10,25 +11,38 @@ function App() {
     <Provider store={store}>
       <Router>
         <div className="min-h-screen">
-          <nav className="bg-gray-800 text-white p-4">
-            <ul className="flex space-x-4">
-              <li>
-                <Link to="/" className="hover:text-gray-300">
-                  Home
+          <div className="bg-gray-800 text-white p-4">
+            <div className="flex items-center justify-between !px-8 !py-2">
+              <div>
+                <Link to="/">
+                  <img src={logoTransparent} alt="Logo" className="!h-8 " />
                 </Link>
-              </li>
-              <li>
-                <Link to="/about-me" className="hover:text-gray-300">
-                  About Me
-                </Link>
-              </li>
-            </ul>
-          </nav>
+              </div>
+              <div className="flex !space-x-4">
+                <div>
+                  <Link to="/" className="hover:text-gray-300">
+                    Home
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/about-me" className="hover:text-gray-300">
+                    About Me
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/Login" className="hover:text-gray-300">
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
 
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about-me" element={<AboutMe />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
